a60b628a4af14b49490ba227492102fa9e108fc1
Cao Quan <quancao08@gmail.com>
2025-02-13 14:17:11 +0700

    update blog sidebar

--- fpt-matching/src/app/blog/page.tsx
+++ fpt-matching/src/app/blog/page.tsx
@@ -39,12 +39,150 @@ export default function Blog() {
   };
 
   return (
-    <div className='bg-slate-300'>
-      <div className='blog-center flex flex-row max-w-screen-2xl h-auto mx-auto bg-slate-300 '>
-        <div className='blog-left basis-1/6 bg-slate-300'> a </div>
+    <div className='bg-slate-200'>
+      <div className='blog-center flex flex-row max-w-screen-2xl h-auto mx-auto bg-slate-200 '>
+        {/* blog left */}
+        <div className='blog-left basis-1/6 bg-slate-200'>
+          <aside className="hidden w-64 md:block min-h-screen">
+            <div className="py-3 text-2xl items-start bg-white border-b-2 mb-6 mt-5 mx-3  px-3">
+              <div className="font-bold text-xl">DEV Community is a community of 2,827,832 amazing developers</div>
+              <div className='text-sm mt-2'>We're a place where coders share, stay up-to-date and grow their careers.</div>
+              <a href=""> 
+              <div className='Login w-full mt-2 text-center border-2 p-1 text-xl border-blue-700 hover:bg-blue-700 hover:text-white hover:underline'> Login</div>
+              </a>
+              <a href="">
+              <div className='Register w-full mt-2 text-center  p-1 text-xl  hover:bg-blue-200 hover:underline'> Register</div>
+              </a>
+            </div>
+            <nav className="text-sm ">
+              <ul className="flex flex-col">
+                <li className="px-4 cursor-pointer bg-gray-500 text-gray-800 hover:bg-blue-300  hover:text-white">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                    </svg>
+
+                    Trang chủ
+                  </a>
+                </li>
+                <li className="px-4 py-2 text-xs uppercase tracking-wider text-gray-500 font-bold">USER MANAGEMENT</li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                    </svg>
+                    Users
+                  </a>
+                </li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                    Roles
+                  </a>
+                </li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                    </svg>
+
+                    Permissions
+                  </a>
+                </li>
+                <li className="px-4 py-2 text-xs uppercase tracking-wider text-gray-500 font-bold">Blog Management</li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
+                    </svg>
+
+                    Blog Cá nhân
+                  </a>
+                </li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+
+                    Blog Sharing
+                  </a>
+                </li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a className="py-3 flex items-center" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+
+                    Blog Project
+                  </a>
+                </li>
+              
+         
+                <li className="px-4 py-2 text-xs uppercase tracking-wider text-gray-500 font-bold">INFORMATION MANAGEMENT</li>
+                <li className="px-4 hover:bg-blue-300">
+                  <a href="#" className="py-3 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
+                    </svg>
+                    Reports
+                  </a>
+                </li>
+                <li className="px-4 py-2 mt-2 text-xs uppercase tracking-wider text-gray-500 font-bold">Apps</li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a href="#" className="py-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+                    </svg>
+
+                    Messages
+                    <span className="ml-auto text-xs bg-gray-500 px-2 py-1 rounded-sm">16</span>
+                  </a>
+                </li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a href="#" className="py-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+                    </svg>
+
+                    Notification
+                    <span className="ml-auto text-xs bg-gray-500 px-2 py-1 rounded-sm">16</span>
+                  </a>
+                </li>
+                <li className="px-4 cursor-pointer hover:bg-blue-300">
+                  <a href="#" className="py-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 mr-3">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                    </svg>
+
+                    Calendar
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+        </div>
+        {/* blog center */}
         <div className='blog-center basis-1/2 '>
 
-          <div className='form-create-blog bg-slate-100 rounded-xl w-full max-w-3xl p-3 mx-2'>
+          <div className='form-create-blog bg-slate-100 rounded-xl w-full max-w-3xl p-3 mx-2 mt-3'>
             <form onSubmit={handleSubmit}>
               <div className="flex items-center space-x-3">
                 <img
@@ -81,7 +219,7 @@ export default function Blog() {
             <span className=' px-2  hover:bg-white hover:text-blue-900'>Latest</span>
             <span className='px-2  hover:bg-white hover:text-blue-900'>Top</span>
           </div>
-           {/* Blog */}
+          {/* Blog */}
           <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
             <div>
               {/* Post Header with Avatar, Username, and Date */}
@@ -388,8 +526,8 @@ export default function Blog() {
               </div>
             </div>
           </div>
-            {/* Blog */}
-            <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
+          {/* Blog */}
+          <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
             <div>
               {/* Post Header with Avatar, Username, and Date */}
               <div className="flex items-center space-x-4">
@@ -696,8 +834,8 @@ export default function Blog() {
             </div>
           </div>
 
-            {/* Blog */}
-            <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
+          {/* Blog */}
+          <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
             <div>
               {/* Post Header with Avatar, Username, and Date */}
               <div className="flex items-center space-x-4">
@@ -1003,8 +1141,8 @@ export default function Blog() {
               </div>
             </div>
           </div>
-            {/* Blog */}
-            <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
+          {/* Blog */}
+          <div className='bg-white max-w-3xl mx-3 my-8 p-6 rounded-xl shadow-md  '>
             <div>
               {/* Post Header with Avatar, Username, and Date */}
               <div className="flex items-center space-x-4">
@@ -1310,8 +1448,9 @@ export default function Blog() {
               </div>
             </div>
           </div>
-      
+
         </div>
+        {/* blog right */}
         <div className='blog-right basis-2/6'>
           <div className='box-title'>
             <div className=" w-full relative max-w-xs mx-3 m-3">
