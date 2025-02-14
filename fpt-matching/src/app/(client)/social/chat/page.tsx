bda90bda3961deb5ab958ea2e01e0701570235ab
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-02-14 17:03:44 +0700

    fix chat

---
+++ fpt-matching/src/app/(client)/social/chat/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+export default function Page() {
+  return (
+    <main
+      className="relative flex justify-center items-center flex-col
+   "
+    >
+      <div className="container mx-auto">
+        <div className="h-[1999px]">
+          <h1 className="text-4xl font-bold">Chat Page</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
