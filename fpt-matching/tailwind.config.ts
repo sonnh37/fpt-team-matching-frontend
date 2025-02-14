3b7cd8b77951b44a7e5132fe002fad4e236a730b
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-02-14 16:07:24 +0700

    Add border radius tailwind card xl

--- fpt-matching/tailwind.config.ts
+++ fpt-matching/tailwind.config.ts
@@ -74,6 +74,7 @@ const config: Config = {
   			}
   		},
   		borderRadius: {
+			xl: 'calc(var(--radius) + 4px)',
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
