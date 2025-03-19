224ebb7f83f6c3076afb94ad9a683c3a70170394
Cao Quan <quancao08@gmail.com>
2025-03-20 01:01:46 +0700

    show comment va dang comment

--- fpt-matching/src/components/_common/comment/comment.tsx
+++ fpt-matching/src/components/_common/comment/comment.tsx
@@ -1,14 +1,28 @@
-import { DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { Popover } from '@/components/ui/popover'
-import { faAngleDown, faEarthAmericas } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faEarthAmericas, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
-import { PopoverContent, PopoverTrigger } from '@radix-ui/react-popover'
-import React, { useEffect } from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import React, { useEffect, useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { useQuery } from '@tanstack/react-query'
 import { commentService } from '@/services/comment-service'
 import { CommentGetAllQuery } from '@/types/models/queries/comment/comment-get-all-query'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/lib/redux/store'
+import { CommentCreateCommand } from '@/types/models/commands/comment/comment-create-command'
+import { toast } from 'sonner'
+import { ModalFooter } from '@/components/ui/animated-modal'
 
 
 interface CommentBlogProps {
@@ -16,10 +30,17 @@ interface CommentBlogProps {
 }
 
 const CommentBlog: React.FC<CommentBlogProps> = ({ id }) => {
-    const query: CommentGetAllQuery = {
-        projectId: id
-    };
+    const [commentUser, setComment] = useState("");
+    const user = useSelector((state: RootState) => state.user.user)
 
+    const [postId, setPostId] = useState(id);
+
+    console.log(id, "test day ne")
+
+    const query: CommentGetAllQuery = {
+        blogId: postId
+    };
+    console.log(query, "test day ")
     const {
         data: result,
         refetch,
@@ -29,8 +50,31 @@ const CommentBlog: React.FC<CommentBlogProps> = ({ id }) => {
         queryFn: () => commentService.fetchAll(query),
         refetchOnWindowFocus: false,
     });
+
+    useEffect(() => {
+        if (postId) {
+            refetch();
+        }
+    }, [postId, refetch]);
+
+
+
+    async function handleComment(comment: string) {
+        const query: CommentCreateCommand = {
+            userId: user?.id,
+            content: comment,
+            blogId: postId
+        };
+
+        const result = await commentService.create(query);
+        if (result.status === 1) {
+
+            toast("Bạn đã mới comment thành công")
+            refetch()
+        }
+    }
     return (
-        <div className='blog-comment'>
+        <div className='blog-comment '>
             <div className='filter-comment'>
                 <Popover>
                     <PopoverTrigger asChild>
@@ -60,8 +104,8 @@ const CommentBlog: React.FC<CommentBlogProps> = ({ id }) => {
                 </Popover>
             </div>
 
-            { result?.data?.length === 0 ? (
-                <div className='comment-content w-full px-3 pt-1 flex justify-center '>
+            {result?.data?.length === 0 ? (
+                <div className='comment-content w-full px-3 pt-1 my-5 flex justify-center '>
                     <p className=' text-xl'>Chưa có bình luận nào</p>
                 </div>
             ) : (
@@ -113,6 +157,34 @@ const CommentBlog: React.FC<CommentBlogProps> = ({ id }) => {
             )}
 
 
+            <ModalFooter className="absolute bottom-0 left-0 w-full bg-white mt-2 p-3 border-t">
+                <div className='flex w-full'>
+                    <img
+                        src={user?.avatar ?? "/user-avatardefault.jpg"} // Replace with your avatar image
+                        alt="User Avatar"
+                        className="w-10 h-10 rounded-full"
+                    />
+                    <div className="relative w-full">
+                        <textarea
+                            className="w-full px-2 py-2 pr-10 border rounded-md resize-none"
+                            placeholder="Hãy làm người văn minh đi"
+                            value={commentUser}
+                            onChange={(e) => setComment(e.target.value)} // Cập nhật giá trị state
+                        ></textarea>
+
+                        {/* Nút Send nằm góc phải dưới */}
+                        <button onClick={() => {
+                            if (commentUser.trim() !== "") {
+                                handleComment(commentUser); // Gửi nội dung comment + id của project
+                                setComment(""); // Xóa nội dung textarea sau khi gửi
+
+                            }
+                        }} className="absolute bottom-2 right-2 bg-blue-500 text-white px-3 py-1 text-sm rounded-md hover:bg-blue-600">
+                            <FontAwesomeIcon icon={faPaperPlane} /> Send
+                        </button>
+                    </div>
+                </div>
+            </ModalFooter>
 
 
         </div>
--- fpt-matching/src/types/models/queries/comment/comment-get-all-query.ts
+++ fpt-matching/src/types/models/queries/comment/comment-get-all-query.ts
@@ -2,6 +2,6 @@ import { BaseQueryableQuery } from "../_base/base-query";
 
 export interface CommentGetAllQuery extends BaseQueryableQuery {
     userId?: string,
-    projectId?: string,
+    blogId?: string,
  
 }
