c5e3a1e6de053abbb38b01cf4ef1561f9a1e1d01
Son Nguyen <145078681+sonnh37@users.noreply.github.com>
2025-03-28 00:42:21 +0700

    Update ui login

--- fpt-matching/src/app/(auth)/login-by-account/page.tsx
+++ fpt-matching/src/app/(auth)/login-by-account/page.tsx
@@ -7,8 +7,13 @@ export default function Page() {
   const router = useRouter();
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+    <div className="relative flex min-h-svh items-center justify-center p-6 md:p-10">
+      <div className="absolute inset-0 bg-[url('/dai-hoc-fpt.jpg')] bg-cover bg-center blur-sm"></div>
+
+      {/* Overlay làm tối nền (tuỳ chọn) */}
+      <div className="absolute inset-0 bg-black/50 "></div>
+
+      <div className="relative z-10 w-full max-w-sm md:max-w-3xl">
         <LoginAccountForm />
       </div>
     </div>
