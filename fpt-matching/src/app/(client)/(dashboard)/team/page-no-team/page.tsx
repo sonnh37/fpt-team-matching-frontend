5600e6bfad6edbce564f327d8f675e445c4433b5
Son Nguyen <145078681+sonnh37@users.noreply.github.com>
2025-03-30 19:40:09 +0700

    Update ui create team

--- fpt-matching/src/app/(client)/(dashboard)/team/page-no-team/page.tsx
+++ fpt-matching/src/app/(client)/(dashboard)/team/page-no-team/page.tsx
@@ -1,10 +1,29 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
-import { TbUsersPlus } from "react-icons/tb";
-import { LuUsersRound } from "react-icons/lu";
-import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const PageNoTeam = () => {
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import { LuUsersRound } from "react-icons/lu";
+import { TbUsersPlus } from "react-icons/tb";
+import { TeamForm } from "@/components/sites/team/create";
+export const PageNoTeam = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const showCreateModal = searchParams.get("create") === "true";
+
+  const handleOpenChange = (open: boolean) => {
+    if (open) {
+      router.replace("/team?create=true");
+    } else {
+      router.replace("/team");
+    }
+  };
+
   return (
     <div className="absolute inset-0 flex justify-center items-center">
       <div className="flex flex-col gap-4">
@@ -12,11 +31,21 @@ const PageNoTeam = () => {
           You don't have any team. Let's find your team with options:
         </div>
         <div className="w-full flex justify-center">
-          <Button variant={"default"} asChild>
-            <Link href={"/team/create"}>
-              <TbUsersPlus /> Create team
-            </Link>
-          </Button>
+          <Dialog open={showCreateModal} onOpenChange={handleOpenChange}>
+            <DialogTrigger asChild>
+              <Button variant={"default"}>
+                <TbUsersPlus /> Create team
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="">
+              <DialogHeader>
+                <DialogTitle>New project</DialogTitle>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <TeamForm />
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="w-full flex justify-center">
           <Button variant={"default"}>
@@ -27,5 +56,3 @@ const PageNoTeam = () => {
     </div>
   );
 };
-
-export default PageNoTeam;
--- fpt-matching/src/components/sites/team/create/index.tsx
+++ fpt-matching/src/components/sites/team/create/index.tsx
@@ -2,40 +2,19 @@
 import { useForm } from "react-hook-form";
 
 import { Card, CardContent } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { usePreviousPath } from "@/hooks/use-previous-path";
+import { FormInput } from "@/lib/form-custom-shadcn";
+import { projectService } from "@/services/project-service";
+import { TeamCreateCommand } from "@/types/models/commands/projects/team-create-command";
+import { Project } from "@/types/project";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 import { z } from "zod";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { HeaderForm } from "@/components/_common/create-update-forms/header-form";
-import { InformationBaseCard } from "@/components/_common/create-update-forms/information-base-form";
-import { usePreviousPath } from "@/hooks/use-previous-path";
-import ConfirmationDialog, { FormInput } from "@/lib/form-custom-shadcn";
-import { BusinessResult } from "@/types/models/responses/business-result";
-import { useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { DataOnlyTable } from "@/components/_common/data-table-client/data-table";
-import { TeamCreateCommand } from "@/types/models/commands/projects/team-create-command";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Project } from "@/types/project";
-import { projectService } from "@/services/project-service";
-interface TeamFormProps {
-  initialData?: Project | null;
-}
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
   teamName: z
@@ -43,13 +22,12 @@ const formSchema = z.object({
     .min(2, { message: "English Title must be at least 2 characters." }),
   teamSize: z
     .number({ invalid_type_error: "Team size must be a number." })
-    .gte(2, { message: "Team size must be at least 2." }).default(4),
+    .gte(1, { message: "Team size must be at least 1." })
+    .default(1),
 });
 
-export const TeamForm: React.FC<TeamFormProps> = ({ initialData = null }) => {
+export const TeamForm = () => {
   const [loading, setLoading] = useState(false);
-  const title = initialData ? "Edit team" : "Create team";
-  const action = initialData ? "Save and continue" : "Create";
   const [firebaseLink, setFirebaseLink] = useState<string | null>(null);
   const router = useRouter();
   const [showConfirmationDialog, setShowConfirmationDialog] = useState(false);
@@ -63,37 +41,22 @@ export const TeamForm: React.FC<TeamFormProps> = ({ initialData = null }) => {
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData
-      ? {
-          ...initialData,
-        }
-      : {},
+    defaultValues: { teamSize: 1 },
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
-      if (initialData) {
-        // #not-yet-implemented
-        // const updatedValues: TeamCreateCommand = {
-        //   teamName: values.teamName,
-        //   teamSize: values.teamSize,
-        // };
-        // const response = await projectService.update(updatedValues);
-        // if (response.status != 1) throw new Error(response.message);
-        // toast.success(response.message);
-        // router.push(previousPath);
-      } else {
-        const createdValues: TeamCreateCommand = {
-          teamName: values.teamName,
-          teamSize: values.teamSize,
-        };
-        const response = await projectService.createTeam(createdValues);
-        if (response.status !== 1) throw new Error(response.message);
+      const createdValues: TeamCreateCommand = {
+        teamName: values.teamName,
+        teamSize: 1,
+      };
+      const response = await projectService.createTeam(createdValues);
+      if (response.status !== 1) throw new Error(response.message);
 
-        toast.success(response.message);
-        // router push
-      }
+      toast.success(response.message);
+      await queryClient.refetchQueries({ queryKey: ["getTeamInfo"] });
+      router.push("/team");
     } catch (error: any) {
       console.error(error);
       toast.error(error.message);
@@ -105,76 +68,17 @@ export const TeamForm: React.FC<TeamFormProps> = ({ initialData = null }) => {
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-4">
-          <div className="grid gap-2">
-            <HeaderForm
-              previousPath={previousPath}
-              title={title}
-              initialData={initialData}
-              loading={loading}
-              action={action}
-            />
-          </div>
-          <div className="grid gap-4">
-            <div className="grid gap-4 lg:grid-cols-3">
-              <div className="grid gap-4 lg:col-span-2">
-                {/* main */}
-                <Card className="overflow-hidden">
-                  <CardContent className="p-6">
-                    <div className="grid gap-6">
-                      <div className="grid gap-3">
-                        <FormInput
-                          form={form}
-                          name="teamName"
-                          label="Team Name"
-                          placeholder="Enter name"
-                        />
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormInput
+            form={form}
+            name="teamName"
+            label="Team Name"
+            placeholder="Enter name"
+          />
 
-                        <FormField
-                          control={form.control}
-                          name="teamSize"
-                          render={({ field }) => (
-                            <FormItem>
-                              <FormLabel>Team size</FormLabel>
-                              <FormControl>
-                                <Select
-                                  onValueChange={(value) => {
-                                    field.onChange(Number(value));
-                                  }}
-                                  value={field.value?.toString()}
-                                >
-                                  <SelectTrigger>
-                                    <SelectValue
-                                      placeholder={"Select team size"}
-                                    />
-                                  </SelectTrigger>
-                                  <SelectContent>
-                                    {[4, 5, 6].map((option) => (
-                                      <SelectItem
-                                        key={option}
-                                        value={option.toString()}
-                                      >
-                                        {option}
-                                      </SelectItem>
-                                    ))}
-                                  </SelectContent>
-                                </Select>
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-
-              <div className="grid gap-4 h-fit">
-                <InformationBaseCard form={form} initialData={initialData} />
-              </div>
-            </div>
-          </div>
+          <Button className="w-fit" type="submit">
+            Create
+          </Button>
         </form>
       </Form>
     </>
