e5cb52c466b01d6f329192be2f949046e9cd885b
Cao Quan <quancao08@gmail.com>
2025-04-05 09:08:19 +0700

    gan api va ui create rate va check cac dk

--- fpt-matching/src/app/(client)/(dashboard)/team/rate/page.tsx
+++ fpt-matching/src/app/(client)/(dashboard)/team/rate/page.tsx
@@ -26,19 +26,12 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 import RateStudent from '@/components/_common/rate-student/rate-student';
+import { rateService } from '@/services/rate-service';
 
 export default function Page() {
 
-    const [formData, setformData] = useState({
-        rateById: "",
-        rateForId: "",
-        numOfStar: 1,
-        content: ""
-    });
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
 
-    };
+
 
     //gọi thông tin user đã đăng nhập
     const user = useSelector((state: RootState) => state.user.user)
@@ -53,22 +46,26 @@ export default function Page() {
     //   Loc member ngoai thang danh gia
     const member = result?.data?.teamMembers.filter(x => x.userId != user?.id)
 
-    const handlSubmit = async () => {
 
-    }
+    const { data: ratingData } = useQuery({
+        queryKey: ["getAllRates"],
+        queryFn: () => rateService.fetchAll(), // API trả về danh sách các đánh giá
+        refetchOnWindowFocus: false,
+    });
 
+    console.log(ratingData, "test")
 
 
     return (
         <div>
             <h1 className='font-semibold text-2xl ml-4'>Danh sách đánh giá </h1>
             <Table className='ml-4 mt-5 text-lg'>
-                <TableCaption>Danh sách nộp ứng tuyển.</TableCaption>
+                <TableCaption>Danh sách thành viên trong nhóm.</TableCaption>
                 <TableHeader>
                     <TableRow className=''>
                         <TableHead className="w-[100px]">Số thứ tự</TableHead>
                         <TableHead className="">Tên nhóm</TableHead>
-                        <TableHead className="">Chuyển ngành</TableHead>
+                        <TableHead className="">Chuyên ngành</TableHead>
                         <TableHead className="">Người hướng dẫn</TableHead>
                         <TableHead>Tên thành viên</TableHead>
                         <TableHead>Email</TableHead>
@@ -76,24 +73,41 @@ export default function Page() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {member?.map((cv, index) => (
-                        <TableRow key={cv.id}>
-                            <TableCell className="font-medium ">{index +1}</TableCell>
-                            <TableCell>{result?.data?.teamName}  </TableCell>
-                            <TableCell>{cv.user?.profileStudent?.specialty?.profession?.professionName}</TableCell>
-                            <TableCell >{result?.data?.idea?.mentorId} </TableCell>
-                            <TableCell >{cv.user?.lastName}{cv.user?.firstName}</TableCell>
-                            <TableCell >  {cv.user?.email}</TableCell>
-                            <TableCell >   <RateStudent id={cv.id ?? ""}/></TableCell>
-                        </TableRow>
-                    ))}
+                    {member?.map((cv, index) => {
+                        const hasRated = ratingData?.data?.some(
+                            (r) => r.rateById === user?.teamMembers.find(x=> x.userId == user?.id)?.id && r.rateForId === cv.id
+                        );
+
+                        return (
+                            <TableRow key={cv.id}>
+                                <TableCell className="font-medium">{index + 1}</TableCell>
+                                <TableCell>{result?.data?.teamName}</TableCell>
+                                <TableCell>
+                                    {cv.user?.profileStudent?.specialty?.profession?.professionName}
+                                </TableCell>
+                                <TableCell>{result?.data?.idea?.mentorId}</TableCell>
+                                <TableCell>
+                                    {cv.user?.lastName} {cv.user?.firstName}
+                                </TableCell>
+                                <TableCell>{cv.user?.email}</TableCell>
+
+                                <TableCell>
+                                    {hasRated ? (
+                                        <span className="text-green-600 font-semibold">Bạn đã đánh giá</span>
+                                    ) : (
+                                        <RateStudent id={cv.id ?? ""} />
+                                    )}
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
                 </TableBody>
                 <TableFooter>
 
                 </TableFooter>
             </Table>
 
-        
+
 
         </div>
 
--- fpt-matching/src/components/_common/rate-student/rate-student.tsx
+++ fpt-matching/src/components/_common/rate-student/rate-student.tsx
@@ -5,23 +5,90 @@ import { RootState } from '@/lib/redux/store';
 import { rateService } from '@/services/rate-service';
 import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@radix-ui/react-dialog';
 import { useQuery } from '@tanstack/react-query';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
-
+import { Rating } from "@mui/material";
+import { userService } from '@/services/user-service';
+import { teammemberService } from '@/services/team-member-service';
+import { RateCreateCommand } from '@/types/models/commands/rate/rate-create-command';
+import { toast } from 'sonner';
+import { ideaHistoryService } from '@/services/idea-history-service';
 
 const RateStudent = ({ id }: { id: string }) => {
+    const [rated, setRated] = React.useState(4);
+    const [rated1, setRated1] = React.useState(4);
+    const [rated2, setRated2] = React.useState(4);
 
-    //gọi thông tin user đã đăng nhập
-    const user = useSelector((state: RootState) => state.user.user)
-    const { data: result } = useQuery({
-        queryKey: ["getBlogById", id],
-        queryFn: () => rateService.fetchById(id),
-        refetchOnWindowFocus: false,
-    });
+        //gọi thông tin user đã đăng nhập
+        const user = useSelector((state: RootState) => state.user.user)
 
+        const { data: result } = useQuery({
+            queryKey: ["getMemberById", id],
+            queryFn: () => teammemberService.fetchById(id),
+            refetchOnWindowFocus: false,
+        });
+        // const userisrated = result?.data?.find(x=>x.userId === id)
+    
+    // cập nhật giá trị numOfStar vào formData khi rated thay đổi
+    React.useEffect(() => {
+        setFormData((prev) => ({
+            ...prev,
+            numbOfStar: Math.round((rated + rated1 + rated2) / 3 * 10) / 10,
+        }));
+    }, [rated, rated1, rated2]); // Chạy lại khi giá trị thay đổi
+
+    // cập nhập id khi bấm vào đánh giá
+    React.useEffect(() => {
+        setFormData((prev) => ({
+            ...prev,
+            rateForId: id,
+        }));
+    }, [id]); // chạy lại khi giá trị thay đổi
+
+    const [formData, setFormData] = useState({
+        rateById: user?.teamMembers.find(x => x.userId === user?.id)?.id ?? "", // hoặc x.id nếu đó là teamMemberId
+        rateForId: id,
+        numbOfStar: 1,
+        content: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+
+
+    const handlSubmit = async () => {
+        let query: RateCreateCommand = {
+            rateById: formData.rateById ?? "",
+            rateForId: formData.rateForId,
+            numbOfStar: formData.numbOfStar,
+            content: formData.content
+        }
+
+        const result1 = await rateService.create(query)
+        if (result1?.status == 1) {
+            toast.success("Bạn đã đánh giá thành công!")
+            setFormData({   
+            rateById: user?.teamMembers.find(x => x.userId === user?.id)?.id ?? "", // hoặc x.id nếu đó là teamMemberId
+            rateForId: id,
+            numbOfStar: 1,
+            content: ""})
+        } else {
+            toast.error("Đánh giá thất bại!")
+        }
+
+
+
+    }
     return (
         <div>
-            <div className="hidden" >{result?.data?.id}</div>
+            {/* <div className="hidden" >{result?.data?.id}</div> */}
             {/* <Dialog> */}
             {/* <DialogTrigger asChild>
                     <span className="flex items-center mr-3">
@@ -97,71 +164,112 @@ const RateStudent = ({ id }: { id: string }) => {
 
 
             <Modal>
-                <ModalTrigger className="=">
-                    <button className='bg-blue-400 p-2 rounded-md' >Đánh giá</button>
+                <ModalTrigger className="px-0">
+                    <button className='bg-orange-500 p-2 rounded-md' >Đánh giá</button>
                 </ModalTrigger>
 
-                <ModalBody className="min-h-[80%] max-h-[90%] min-w-[30%] md:max-w-[40%] overflow-auto">
-                    <ModalContent className='flex flex-col text-center' >
-                     
-                            <h2 className="text-2xl font-bold mb-4">Feedback Form</h2>
-                            {/* Name Field */}
-                            <div className="mb-4  text-base text-left">
-                                <label htmlFor="name" className="block mb-1 ml-4 text-base">Tên người đánh giá</label>
-                                <div
-                                    id="name"
-                                    className="w-full py-2 px-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-4"
-                                >{user?.email}</div>
-                            </div>
+                <ModalBody className="min-h-[70%] max-h-[90%] min-w-[30%] md:max-w-[40%] overflow-auto">
+                    <div className='flex flex-col items-center text-center bg-orange-500 p-3  shadow-lg w-full'>   {/* Title */}
+                        <h2 className="text-2xl w-full font-bold text-white">Feedback Form</h2></div>
+                    <ModalContent className="flex flex-col items-center text-center bg-white p-6 rounded-lg shadow-lg w-full">
 
-                            {/* Email Field */}
-                              <div className="mb-4  text-base text-left">
-                                <label htmlFor="name" className="block mb-1 ml-4  text-base">Người nhận đánh giá</label>
-                                <div
-                                    id="name"
-                                    className="w-full py-2 px-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-4"
-                                >{user?.email}</div>
-                            </div>
 
-                            {/* Rating Field */}
-                            <div className="mb-4">
-                                <label className="block text-left mb-1 ml-4">Đánh giá</label>
-                                <div className="flex items-center space-x-2 ml-4">
-                                    {[1, 2, 3, 4, 5].map((num) => (
-                                        <div key={num} className="flex items-center space-x-1">
-                                            <input
-                                                type="radio"
-                                                name="rating"
-                                                id={`rating${num}`}
-                                                value={num}
-                                                className="focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                            />
-                                            <label htmlFor={`rating${num}`}>{num}</label>
-                                        </div>
-                                    ))}
-                                </div>
+                        {/* Name Field */}
+                        <div className="w-full mb-4 text-left">
+                            <label htmlFor="name" className="block font-semibold mb-2">Tên người đánh giá:</label>
+                            <div className="w-full min-h-14  py-2 px-4 rounded border border-orange-500 text-gray-500 font-sans bg-orange-50">
+                                {user?.email}
                             </div>
+                        </div>
 
-                            {/* Message Field */}
-                            <div className="mb-4">
-                                <label htmlFor="message" className="block mb-1 text-left ml-4">Đóng góp ý kiến</label>
-                                <textarea
-                                    id="message"
-                                    className="w-full py-2 px-4 ml-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                ></textarea>
+                        {/* Recipient Field */}
+                        <div className="w-full mb-4 text-left">
+                            <label htmlFor="recipient" className="block  font-semibold mb-2">Người nhận đánh giá:</label>
+                            <div className="w-full min-h-14 py-2 px-4 rounded border border-orange-500  text-gray-500 font-sans font-medium bg-orange-50">
+                                {result?.data?.user?.email}
                             </div>
-                     
+                        </div>
+
+                        {/* Rating Field */}
+                        <div className="w-full mb-4 text-left">
+                            <label className="block  font-semibold mb-2">Đánh giá:</label>
+                            <div className="flex items-center space-x-2  font-bold">
+
+                                <Rating
+                                    name="rating"
+                                    value={rated}
+                                    onChange={(event, value) => setRated(value ?? 1)}
+                                    className="text-orange-500"
+                                />
+                                <span>{rated}</span>
+                                <label className="block  font-semibold ">Teamworking</label>
+
+                            </div>
+                            <div className="flex items-center space-x-2  font-bold">
+
+                                <Rating
+                                    name="rating"
+                                    value={rated1}
+                                    onChange={(event, value) => setRated1(value ?? 1)}
+                                    className="text-orange-500"
+                                />
+                                <span>{rated1}</span>
+                                <label className="block  font-semibold ">Good knownledge/Skills</label>
+
+                            </div>
+                            <div className="flex items-center space-x-2  font-bold">
+
+                                <Rating
+                                    name="rating"
+                                    value={rated2}
+                                    onChange={(event, value) => setRated2(value ?? 1)}
+                                    className="text-orange-500"
+                                />
+                                <span>{rated2}</span>
+                                <label className="block font-semibold ">HardWorking</label>
+
+                            </div>
+                            <div className="block  font-semibold mt-2 "
+                            >Tổng điểm: {(Math.round((rated + rated1 + rated2) / 3 * 10) / 10)}
+                            </div>
+                            {/* Nếu bạn cần lưu giá trị vào formData, hãy dùng input hidden */}
+                            <input
+                                type="hidden"
+                                name="numbOfStar"
+                                value={Math.round((rated + rated1 + rated2) / 3 * 10) / 10}
+                                onChange={handleChange}
+                            />
+
+                        </div>
+
+                        {/* Message Field */}
+                        <div className="w-full mb-4 text-left">
+                            <label htmlFor="message" className="block  font-semibold mb-2">Đóng góp ý kiến:</label>
+                            <textarea
+                                name='content'
+                                className="w-full h-24 py-2 px-4 rounded border border-orange-500  focus:outline-none focus:ring-2 focus:ring-orange-500 bg-orange-50"
+                                placeholder="Nhập ý kiến của bạn..."
+                                value={formData.content}
+                                onChange={handleChange}
+                            ></textarea>
+                        </div>
+
 
                     </ModalContent>
                     <ModalFooter className='text-base justify-end'>
-                        {/* Submit Button */}
-                        <div                            className="  text-white   "
-                        >
-                           <button type='submit' className='py-2 px-2 hover:bg-blue-600 bg-blue-500'>Submit</button> 
+                        <div className="flex space-x-2">
+
+                            {/* Submit Button */}
+                            <button onClick={() => handlSubmit()} className="px-4 py-2 bg-orange-500 text-white rounded-lg font-semibold hover:bg-orange-600 transition">
+                                Gửi Feedback
+                            </button>
+
+                            <ModalClose className='px-4 py-2 bg-gray-300 text-black rounded-lg font-semibold hover:bg-gray-400 transition'>
+                                Không, cảm ơn
+                            </ModalClose>
                         </div>
-                        <ModalClose>   <button className='py-2 px-2 bg-gray-300 rounded hover:bg-gray-600'>Không, cảm ơn</button></ModalClose>
-                     
                     </ModalFooter>
+
                 </ModalBody>
 
             </Modal>
