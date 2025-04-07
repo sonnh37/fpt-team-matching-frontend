ff9931463e6e5defa897229d08425e31eebcaece
loctodale <thang336655@gmail.com>
2025-04-07 22:21:43 +0700

    add show similarity

--- fpt-matching/src/components/sites/idea/approval/council/pending/columns.tsx
+++ fpt-matching/src/components/sites/idea/approval/council/pending/columns.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { DataTableColumnHeader } from "@/components/_common/data-table-api/data-table-column-header";
-import { DeleteBaseEntitysDialog } from "@/components/_common/delete-dialog-generic";
 import ErrorSystem from "@/components/_common/errors/error-system";
 import { LoadingComponent } from "@/components/_common/loading-page";
 import { TypographyP } from "@/components/_common/typography/typography-p";
@@ -16,39 +15,28 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Textarea } from "@/components/ui/textarea";
-import { RootState } from "@/lib/redux/store";
 import { ideaRequestService } from "@/services/idea-request-service";
 import { ideaService } from "@/services/idea-service";
-import { IdeaStatus } from "@/types/enums/idea";
 import { IdeaRequestStatus } from "@/types/enums/idea-request";
 import { Idea } from "@/types/idea";
 import { IdeaRequest } from "@/types/idea-request";
-import { IdeaRequestUpdateCommand } from "@/types/models/commands/idea-requests/idea-request-update-command";
 import { IdeaRequestUpdateStatusCommand } from "@/types/models/commands/idea-requests/idea-request-update-status-command";
-import { IdeaUpdateCommand } from "@/types/models/commands/idea/idae-update-command";
-import { IdeaUpdateStatusCommand } from "@/types/models/commands/idea/idea-update-status-command";
-import { IdeaRequestGetAllCurrentByStatusAndRolesQuery } from "@/types/models/queries/idea-requests/idea-request-get-all-current-by-status-and-roles";
-import { User } from "@/types/user";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { ColumnDef, Row } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
-import { CiFolderOn, CiFolderOff } from "react-icons/ci";
-import { useSelector } from "react-redux";
 import { toast } from "sonner";
 import {IdeaDetailForm} from "@/components/sites/idea/detail";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {Label } from "@/components/ui/label";
+import {Select, SelectValue } from "@/components/ui/select";
 export const columns: ColumnDef<IdeaRequest>[] = [
   {
     accessorKey: "idea.englishName",
@@ -216,7 +204,7 @@ const Actions: React.FC<ActionsProps> = ({ row }) => {
             </DialogTrigger>
             <DialogContent className="sm:min-w-[60%] sm:max-w-fit max-h-screen overflow-y-auto">
               <DialogHeader>
-                <DialogTitle>Idea detail</DialogTitle>
+                <DialogTitle>Idea detail1</DialogTitle>
                 <DialogDescription></DialogDescription>
               </DialogHeader>
               <div className="grid p-4 space-y-24">
