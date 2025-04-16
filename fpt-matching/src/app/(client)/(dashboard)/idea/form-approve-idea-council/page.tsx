ddfbac4cdcae385179601f3167a5a59213e7be8d
Cao Quan <quancao08@gmail.com>
2025-04-16 14:37:12 +0700

    them sidebar manage-criteria va form criteria

--- fpt-matching/src/app/(client)/(dashboard)/idea/form-approve-idea-council/page.tsx
+++ fpt-matching/src/app/(client)/(dashboard)/idea/form-approve-idea-council/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 import React from 'react'
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Textarea } from "@/components/ui/textarea"
 
 const FormCouncil = () => {
 
@@ -11,22 +14,22 @@ const FormCouncil = () => {
         {
             id: 'q2',
             text: "Ngữ cảnh (context) nơi sản phẩm được triển khai có được xác định cụ thể?",
-        },      {
+        }, {
             id: 'q3',
             text: "Vấn đề cần giải quyết (problem statement) có được mô tả rõ ràng là động lực cho việc ra đời của sản phẩm?  ",
-        },      {
+        }, {
             id: 'q4',
             text: "Người dùng chính (main actors) có được xác định trong đề tài?    ",
-        },      {
+        }, {
             id: 'q5',
             text: "Các luồng xử lý chính (main flows) và các chức năng chính (main usescases)  của người dùng có được mô tả ?",
-        },      {
+        }, {
             id: 'q6',
             text: "Khách hàng/người tài trợ (customers/sponsors) của đề tài có được xác định?      ",
-        },      {
+        }, {
             id: 'q7',
             text: "Hướng tiếp cận (Approach) về lý thuyết (theory),công nghệ áp dụng (applied technology)   và các sản phẩm cần tạo ra trong đề tài (main deliverables) có được xác định và phù hợp?        ",
-        },    {
+        }, {
             id: 'q8',
             text: "Phạm vi đề tài (Scope) và độ lớn của sản phẩm (size of product) có khả thi và phù hợp cho nhóm (3-5) SV thực hiện trong 14 tuần ? Có phân chia thành các gói packages để đánh giá?   ",
         },
@@ -45,7 +48,7 @@ const FormCouncil = () => {
 
     return (
         <div className="min-h-screen bg-gray-50 py-8 px-4">
-            <form  className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md">
+            <form className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md">
                 <h1 className="text-2xl font-bold text-center mb-8 text-blue-600">
                     Đánh giá đề tài Capstone Project
                 </h1>
@@ -60,23 +63,32 @@ const FormCouncil = () => {
                         </div>
 
                         <div className="flex gap-4">
-                            <button
-                                type="button"
-                            
-                           
-                            >
-                                Có (Y)
-                            </button>
-                            <button
-                                type="button"
-                               
-                            >
-                                Không (N)
-                            </button>
+                            <RadioGroup defaultValue="comfortable">
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="default" id="r1" />
+                                    <Label htmlFor="r1">Có</Label>
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="comfortable" id="r2" />
+                                    <Label htmlFor="r2">Không</Label>
+                                </div>
+
+                            </RadioGroup>
                         </div>
                     </div>
                 ))}
+                <div className="mb-8 p-4 bg-gray-50 rounded-lg">
+                    <div className="flex items-start mb-4">
+                        <span className="text-lg font-semibold mr-2 text-gray-700">
+                            Đánh giá
+                        </span>
+                        <p className="text-gray-700 flex-1"></p>
+                    </div>
 
+                    <div className="flex gap-4">
+                        <Textarea placeholder='Điền feedback đi bạn'></Textarea>
+                    </div>
+                </div>
                 <div className="mt-8 text-center">
                     <button
                         type="submit"
