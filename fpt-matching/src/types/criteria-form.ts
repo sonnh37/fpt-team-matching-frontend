0be93f3de0abd74930557ad4c2ba312fb5162faa
Cao Quan <quancao08@gmail.com>
2025-04-18 21:56:42 +0700

    sua lai mot so loi criteria

--- fpt-matching/src/types/criteria-form.ts
+++ fpt-matching/src/types/criteria-form.ts
@@ -1,7 +1,9 @@
 import { BaseEntity } from "./_base/base";
+import { CriteriaXCriteriaForm } from "./criteria-x-criteriaform";
 
 
 export interface CriteriaForm extends BaseEntity {
   title?: string;
+  criteriaXCriteriaForms: CriteriaXCriteriaForm[];
 
 }
