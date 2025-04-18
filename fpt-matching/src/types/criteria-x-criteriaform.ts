2450ab1e4148c7c0f6a6254038f5e484553afff7
Cao Quan <quancao08@gmail.com>
2025-04-18 21:57:26 +0700

    them command va query

--- fpt-matching/src/types/criteria-x-criteriaform.ts
+++ fpt-matching/src/types/criteria-x-criteriaform.ts
@@ -1,7 +1,9 @@
 import { BaseEntity } from "./_base/base";
+import { Criteria } from "./criteria";
 
 export interface CriteriaXCriteriaForm extends BaseEntity {
     criteriaFormId?: string;
     criteriaId?: string;
+    criteria: Criteria;
   
 }
