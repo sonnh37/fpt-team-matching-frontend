a4a5adf43f79b9e785afc0360df940db9d300386
loctodale <thang336655@gmail.com>
2025-04-21 15:44:09 +0700

    fix update topic

--- fpt-matching/src/lib/constants/const.ts
+++ fpt-matching/src/lib/constants/const.ts
@@ -36,5 +36,5 @@ export class Const {
   static readonly USER_X_ROLES = "user-x-roles";
   static readonly ROLES = "roles";
   static readonly TOPIC_VERSIONS = "topic-versions";
-  static readonly TOPIC_VERSIONS_REQUEST = "topic-version-request";
+  static readonly TOPIC_VERSIONS_REQUEST = "topic-version-requests";
 }
