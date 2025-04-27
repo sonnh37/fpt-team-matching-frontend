7210fb7b5b0c6992e3efd2d5940d4b0ca3be1f18
Cao Quan <quancao08@gmail.com>
2025-04-27 19:28:25 +0700

    sua list upload va them suggest

--- fpt-matching/src/components/_common/listupload/list-upload.tsx
+++ fpt-matching/src/components/_common/listupload/list-upload.tsx
@@ -7,7 +7,12 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs-shadcn";
 import {
     Table,
     TableBody,
@@ -31,9 +36,16 @@ import { invitationService } from "@/services/invitation-service";
 import { userService } from "@/services/user-service";
 import { projectService } from "@/services/project-service";
 import { TeamInvitationCommand } from "@/types/models/commands/invitation/invitation-team-command";
+import { apiHubsService } from "@/services/api-hubs-service";
 
 const ListUploadCv = ({ blogId }: { blogId: string }) => {
 
+
+
+    const tabs = {
+        form: "Gợi ý người dùng phù hợp",
+        upload: "Quản lí các đơn nộp vào team  "
+    };
 
     let query: BlogCvGetAllQuery = {
         blogId: blogId,
@@ -47,6 +59,30 @@ const ListUploadCv = ({ blogId }: { blogId: string }) => {
         queryFn: () => blogCvService.getAll(query),
         refetchOnWindowFocus: false,
     });
+
+
+    const {
+        data: blog,
+    } = useQuery({
+        queryKey: ["getBlogById", blogId],
+        queryFn: () => blogService.getById(blogId),
+        refetchOnWindowFocus: false,
+    });
+
+    console.log(blog,"test0 ")
+
+
+
+    const {
+        data: recommend,
+    } = useQuery({
+        queryKey: ["apiHubService", blog?.data?.skillRequired],
+        queryFn: () => apiHubsService.getRecommendUsers(blogId),
+        refetchOnWindowFocus: false,
+    });
+
+                
+    console.log(recommend,"test ")
 
 
 
@@ -123,6 +159,7 @@ const ListUploadCv = ({ blogId }: { blogId: string }) => {
 
     return (
         <Dialog>
+
             <DialogTrigger asChild>
                 <span className="flex items-center mr-3">
                     <i className="fas fa-image text-red-500"></i>
@@ -130,68 +167,120 @@ const ListUploadCv = ({ blogId }: { blogId: string }) => {
                 </span>
             </DialogTrigger>
             <DialogContent className="max-w-max min-w-[450px]">
-                <DialogHeader>
-                    <DialogTitle className="text-lg" >Một số gợi ý sinh viên đạt đủ chỉ tiêu</DialogTitle>
-                    <DialogDescription className="text-base">
-                        Đây là nơi bạn sẽ sẽ xem danh sách học sinh đã nộp ứng tuyển vào team
-                    </DialogDescription>
-                </DialogHeader>
-                
-                <DialogHeader>
-                    <DialogTitle className="text-lg" >Danh sách nộp đơn ứng tuyển</DialogTitle>
-                    <DialogDescription className="text-base">
-                        Đây là nơi bạn sẽ sẽ xem danh sách học sinh đã nộp ứng tuyển vào team
-                    </DialogDescription>
-                </DialogHeader>
-                <Table>
-                    <TableCaption>Danh sách nộp ứng tuyển.</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[100px]">Số thứ tự</TableHead>
-                            <TableHead>Ngày nộp</TableHead>
-                            <TableHead>Tên người nộp</TableHead>
-                            <TableHead>Email</TableHead>
-                            <TableHead>Department</TableHead>
-                            <TableHead className="max-h-[500px] overflow-x-auto whitespace-nowrap">File CV</TableHead>
-                            <TableHead className="text-center">Profile </TableHead>
-                            <TableHead className="text-center">Action </TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {post?.data?.results?.map((cv, index) => (
-                            <TableRow key={cv.id}>
-                                <TableCell className="font-medium">{index}</TableCell>
-                                <TableCell className="font-medium">{cv.createdDate ? new Date(cv.createdDate).toLocaleString("vi-VN", {
-                                    day: "2-digit",
-                                    month: "2-digit",
-                                    year: "numeric",
-                                    hour: "2-digit"
-                                })
-                                    : "Không có ngày "}</TableCell>
-                                <TableCell>{cv.user?.lastName} {cv.user?.firstName}  </TableCell>
-                                <TableCell >{cv.user?.email}</TableCell>
-                                <TableCell>{Department[cv.user?.department ?? 0]}</TableCell>
-                                <TableCell className="max-w-[400px] overflow-x-auto whitespace-nowrap">{cv.fileCv && (
-                                    <a href={cv.fileCv} className="border-b-2 border-blue-500 text-blue-500">
-                                        Link Download
-                                    </a>
-                                )}</TableCell>
-                                <TableCell >   <button className="p-2 bg-orange-400 ml-3 rounded-sm"><a href={`/social/blog/profile-social/${cv.user?.id}`}>Xem profile</a></button></TableCell>
-                                <TableCell >
-                                    <button className="p-2 bg-red-600 ml-3 rounded-sm" onClick={() => handleDelete(cv.id ?? "")}> Xóa CV</button>
-                                    <button className="p-2 bg-blue-600 ml-3 rounded-sm" onClick={() => handleInvite(cv.user?.email ?? "")}> Mời Vô Nhóm</button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                    <TableFooter>
+                <Tabs className="w-full">
+                    <TabsList className="grid grid-cols-2 w-full max-w-md mx-auto mb-6">
+                        <TabsTrigger value={tabs.form} className="data-[state=active]:bg-primary data-[state=active]:text-white">
+                            {tabs.form}
+                        </TabsTrigger>
+                        <TabsTrigger value={tabs.upload} className="data-[state=active]:bg-primary data-[state=active]:text-white">
+                            {tabs.upload}
+                        </TabsTrigger>
+                    </TabsList>
 
-                    </TableFooter>
-                </Table>
-                <DialogFooter>
-                    {/* <Button onClick={() => submit()}>Ứng tuyển</Button> */}
-                    {/* <Button type="cancel">Không, cảm ơn</Button> */}
-                </DialogFooter>
+                    <TabsContent value={tabs.form}>
+                        <DialogHeader>
+                            <DialogTitle className="text-lg" >Một số gợi ý sinh viên đạt đủ chỉ tiêu</DialogTitle>
+                            <DialogDescription className="text-base">
+                                Đây là nơi bạn sẽ sẽ xem danh sách học sinh đã nộp ứng tuyển vào team
+                            </DialogDescription>
+                        </DialogHeader>
+                        <Table>
+                            <TableCaption>Danh sách nộp ứng tuyển.</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Số thứ tự</TableHead>
+                                    <TableHead>Tên người dùng</TableHead>
+                                    <TableHead>Email</TableHead>
+                                    <TableHead className="max-h-[500px] overflow-x-auto whitespace-nowrap">Kỹ năng</TableHead>
+                                    <TableHead className="text-center">Mức độ tương đồng </TableHead>
+                                    <TableHead className="text-center">Hồ sơ </TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {recommend?.map((cv, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell className="font-medium">{index +1}</TableCell>
+                                        <TableCell>{cv.first_name } {cv.last_name} </TableCell>
+                                        <TableCell >{cv.email}</TableCell>
+                                        <TableCell  className="max-h-[500px] overflow-x-auto whitespace-nowrap">{cv.full_skill}</TableCell>
+                                        <TableCell>{} {cv.similarity.toFixed(2) * 100}%</TableCell>
+                                        <TableCell >   <button className="p-2 bg-orange-400 ml-3 rounded-sm"><a href={`/social/blog/profile-social/${cv.user_id}`}>Xem profile</a></button></TableCell>
+                                        {/* <TableCell >
+                                            <button className="p-2 bg-blue-600 ml-3 rounded-sm" onClick={() => handleInvite(cv.email ?? "")}> Mời Vô Nhóm</button>
+                                        </TableCell> */}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+
+                            </TableFooter>
+                        </Table>
+
+                    </TabsContent>
+
+                    <TabsContent value={tabs.upload}>
+                        <DialogHeader>
+                            <DialogTitle className="text-lg" >Danh sách nộp đơn ứng tuyển</DialogTitle>
+                            <DialogDescription className="text-base">
+                                Đây là nơi bạn sẽ sẽ xem danh sách học sinh đã nộp ứng tuyển vào team
+                            </DialogDescription>
+                        </DialogHeader>
+                        <Table>
+                            <TableCaption>Danh sách nộp ứng tuyển.</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Số thứ tự</TableHead>
+                                    <TableHead>Ngày nộp</TableHead>
+                                    <TableHead>Tên người nộp</TableHead>
+                                    <TableHead>Email</TableHead>
+                                    <TableHead>Department</TableHead>
+                                    <TableHead className="max-h-[500px] overflow-x-auto whitespace-nowrap">File CV</TableHead>
+                                    <TableHead className="text-center">Hồ sơ </TableHead>
+                                    <TableHead className="text-center">Hành động </TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {post?.data?.results?.map((cv, index) => (
+                                    <TableRow key={cv.id}>
+                                        <TableCell className="font-medium">{index}</TableCell>
+                                        <TableCell className="font-medium">{cv.createdDate ? new Date(cv.createdDate).toLocaleString("vi-VN", {
+                                            day: "2-digit",
+                                            month: "2-digit",
+                                            year: "numeric",
+                                            hour: "2-digit"
+                                        })
+                                            : "Không có ngày "}</TableCell>
+                                        <TableCell>{cv.user?.lastName} {cv.user?.firstName}  </TableCell>
+                                        <TableCell >{cv.user?.email}</TableCell>
+                                        <TableCell>{Department[cv.user?.department ?? 0]}</TableCell>
+                                        <TableCell className="max-w-[400px] overflow-x-auto whitespace-nowrap">{cv.fileCv && (
+                                            <a href={cv.fileCv} className="border-b-2 border-blue-500 text-blue-500">
+                                                Link Download
+                                            </a>
+                                        )}</TableCell>
+                                        <TableCell >   <button className="p-2 bg-orange-400 ml-3 rounded-sm"><a href={`/social/blog/profile-social/${cv.user?.id}`}>Xem profile</a></button></TableCell>
+                                        <TableCell >
+                                            <button className="p-2 bg-red-600 ml-3 rounded-sm" onClick={() => handleDelete(cv.id ?? "")}> Xóa CV</button>
+                                            <button className="p-2 bg-blue-600 ml-3 rounded-sm" onClick={() => handleInvite(cv.user?.email ?? "")}> Mời Vô Nhóm</button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+
+                            </TableFooter>
+                        </Table>
+                        <DialogFooter>
+                            {/* <Button onClick={() => submit()}>Ứng tuyển</Button> */}
+                            {/* <Button type="cancel">Không, cảm ơn</Button> */}
+                        </DialogFooter>
+                    </TabsContent>
+                </Tabs>
+
+
+
+
+
             </DialogContent>
         </Dialog>
 
