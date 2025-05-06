daab052d5e8dd8f582b6cef3feaf009edf58aaa1
Cao Quan <quancao08@gmail.com>
2025-05-06 18:27:26 +0700

    thêm api changStatus

--- fpt-matching/src/services/blog-service.ts
+++ fpt-matching/src/services/blog-service.ts
@@ -1,6 +1,8 @@
 import { Blog } from "@/types/blog";
 import { BaseService } from "./_base/base-service";
 import { Const } from "@/lib/constants/const";
+import { BusinessResult } from "@/types/models/responses/business-result";
+import axiosInstance from "@/lib/interceptors/axios-instance";
 
 
 
@@ -9,7 +11,18 @@ class BlogService extends BaseService<Blog>{
     constructor() {
         super(Const.BLOGS);
       }
-
+      
+      public changStatusBLog = async ( id: string ): Promise<BusinessResult<Blog>> => {
+        try {
+          const response = await axiosInstance.put<BusinessResult<Blog>>(
+            `${this.endpoint}/change-status-blog?id=${id}`
+          )
+          return response.data;
+        } catch (error) {
+          return this.handleError(error);
+        }
+      };
+    
       
 }
 
