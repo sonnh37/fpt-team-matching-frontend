ffb4bba662c86b23032254e4e52cd82ef8f2d6a7
Cao Quan <quancao08@gmail.com>
2025-05-23 11:47:18 +0700

    thêm bảng dashboard managementsubmit

---
+++ fpt-matching/src/components/sites/management/projects/manage-submit/index.tsx
@@ -0,0 +1,43 @@
+import { DataOnlyTable } from "@/components/_common/data-table-client/data-table";
+import { topicService } from "@/services/topic-service";
+import { TopicStatus } from "@/types/enums/topic";
+import { TopicGetCurrentByStatusQuery } from "@/types/models/queries/topics/topic-get-current-by-status";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { z } from "zod";
+import { columns } from "./columns";
+import { projectService } from "@/services/project-service";
+import { ProjectGetAllQuery } from "@/types/models/queries/projects/project-get-all-query";
+import { ProjectStatus } from "@/types/enums/project";
+
+//#region INPUT
+const defaultSchema = z.object({
+  // englishName: z.string().optional(),
+});
+//#endregion
+export function ManageProjectSubmit() {
+  const queryParams: ProjectGetAllQuery = {
+    status: ProjectStatus.Pending,
+    isPagination: false,
+  };
+
+  const { data, isFetching, error, refetch } = useQuery({
+    queryKey: ["data", queryParams],
+    queryFn: () => projectService.getAll(queryParams),
+    placeholderData: keepPreviousData,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(data?.data?.results,"test")
+
+  if (error) return <div>Error loading data</div>;
+
+  return (
+    <>
+      <div className="space-y-8">
+        <div className="">
+          <DataOnlyTable data={data?.data?.results ?? []} columns={columns} />
+        </div>
+      </div>
+    </>
+  );
+}
