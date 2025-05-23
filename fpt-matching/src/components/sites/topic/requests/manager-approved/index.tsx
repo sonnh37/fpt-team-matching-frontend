ef5b99e90e526eaf5513e6fe5d6b1a8cd93f12f5
Cao Quan <quancao08@gmail.com>
2025-05-23 13:27:04 +0700

    dashboard manager

--- fpt-matching/src/components/sites/topic/requests/manager-approved/index.tsx
+++ fpt-matching/src/components/sites/topic/requests/manager-approved/index.tsx
@@ -1,10 +1,14 @@
 import { DataOnlyTable } from "@/components/_common/data-table-client/data-table";
 import { topicService } from "@/services/topic-service";
-import { TopicStatus } from "@/types/enums/topic";
+import { TopicStatus, TopicType } from "@/types/enums/topic";
 import { TopicGetCurrentByStatusQuery } from "@/types/models/queries/topics/topic-get-current-by-status";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { z } from "zod";
 import { columns } from "./columns";
+import { TopicGetListByStatusAndRoleQuery } from "@/types/models/queries/topics/topic-get-list-by-status-and-roles-query";
+import { TopicGetAllQuery } from "@/types/models/queries/topics/topic-get-all-query";
+import { useCurrentSemester } from "@/hooks/use-current-role";
+
 
 //#region INPUT
 const defaultSchema = z.object({
@@ -12,19 +16,22 @@ const defaultSchema = z.object({
 });
 //#endregion
 export function TopicRequestPendingManagerTable() {
-  const queryParams: TopicGetCurrentByStatusQuery = {
-    statusList: [
-      TopicStatus.ManagerPending,
-    ],
+  const { currentSemester, isLoading } = useCurrentSemester();
+
+  const queryParams: TopicGetAllQuery = {
+    types: [TopicType.Enterprise,TopicType.Lecturer,TopicType.Lecturer],
+    status:   TopicStatus.ManagerPending,
+    semesterId: currentSemester?.id,
     isPagination: false,
   };
 
   const { data, isFetching, error, refetch } = useQuery({
     queryKey: ["data", queryParams],
-    queryFn: () => topicService.getCurrentTopicOfMeByStatus(queryParams),
+    queryFn: () => topicService.getAll(queryParams),
     placeholderData: keepPreviousData,
     refetchOnWindowFocus: false,
   });
+  console.log(data, "TestTopic")
 
   if (error) return <div>Error loading data</div>;
 
@@ -32,7 +39,7 @@ export function TopicRequestPendingManagerTable() {
     <>
       <div className="space-y-8">
         <div className="">
-          <DataOnlyTable data={data?.data ?? []} columns={columns} />
+          <DataOnlyTable data={data?.data?.results ?? []} columns={columns} />
         </div>
       </div>
     </>
