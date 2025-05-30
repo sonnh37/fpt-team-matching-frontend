40f2038c3d7f4dc9b2448d899efd9fd719f21195
loctodale <thang336655@gmail.com>
2025-05-30 18:40:55 +0700

    update

--- fpt-matching/src/components/sites/topic/supervisors/index.tsx
+++ fpt-matching/src/components/sites/topic/supervisors/index.tsx
@@ -101,7 +101,7 @@ export default function TopicsOfSupervisorsTable() {
   });
 
   const table = useReactTable({
-    data: data?.data?.results ?? [],
+    data: data?.data?.results?.filter(x => x.status == TopicStatus.ManagerApproved) ?? [],
     columns,
     pageCount: data?.data?.totalPages ?? 0,
     state: { pagination, sorting, columnFilters, columnVisibility },
